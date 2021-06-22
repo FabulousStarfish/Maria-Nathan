1fc65941c5eaad896a97e63291e68e0acaa038ed
FabulousStarfish <71825170+FabulousStarfish@users.noreply.github.com>
2021-06-22 09:45:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function goTo(name){
+    if (name=='M'){
+        window.location="maria.html";
+    }
+    else{
+        window.location="nathan.html";
+    }
+}
+function back(){
+    window.location="index.html";
+}
